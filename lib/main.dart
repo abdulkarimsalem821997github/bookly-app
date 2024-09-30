a0e87f47b23dbe70d6e63f01ca8bf25b5be16a72
a.salem <abdulkarimsalem1997@gmail.com>
2024-09-30 20:30:13 +0300

    cache newest books

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:clean_arch_bookly_app/constants.dart';
 
-void main() async{
-  runApp(const Bookly());
+void main() async {
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
-  await Hive.openBox(kFuturedBox);
+
+  await Hive.openBox<BookEntity>(kFuturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
+  runApp(const Bookly());
 }
 
 class Bookly extends StatelessWidget {
