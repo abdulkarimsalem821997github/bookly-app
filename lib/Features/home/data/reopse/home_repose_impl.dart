aba46091e98192beb14238ecb5a822210ab877ac
a.salem <abdulkarimsalem1997@gmail.com>
2024-09-30 22:36:05 +0300

    fetch featured books home repo impl

--- lib/Features/home/data/reopse/home_repose_impl.dart
+++ lib/Features/home/data/reopse/home_repose_impl.dart
@@ -1,3 +1,5 @@
+import 'package:clean_arch_bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app/Features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
 
 import 'package:clean_arch_bookly_app/core/errors/failure.dart';
@@ -7,10 +9,24 @@ import 'package:dartz/dartz.dart';
 import '../../domain/repos/home_repo.dart';
 
 class HomeReposeImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeReposeImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var bookList = homeLocalDataSource.fetchFeaturedBooks();
+      if (bookList.isNotEmpty) {
+        return right(bookList);
+      }
+      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
@@ -18,5 +34,4 @@ class HomeReposeImpl extends HomeRepo {
     // TODO: implement fetchNewestBooks
     throw UnimplementedError();
   }
-
 }
