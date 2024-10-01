7031e7e8e6e3cdeab7504bdd85252e67d5d0769e
a.salem <abdulkarimsalem1997@gmail.com>
2024-10-01 23:10:37 +0300

    impliment cubit featch featured booxs

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,21 @@
-
 import 'package:clean_arch_bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arch_bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:clean_arch_bookly_app/core/errors/failure.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoeading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) => emit(FeaturedBooksError(failure.message)),
+      (books) => emit(FeaturedBooksSuccess(books)));
+  }
 }
